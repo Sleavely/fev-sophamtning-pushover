6896fd409e0ad0bef82e0b4f071f846453aceef5
Joakim Hedlund <contact@joakimhedlund.com>
2021-06-09 21:59:24 +0200

    Cleanup - its already documented in the docs, doh

--- src/utils/pushoverClient.js
+++ src/utils/pushoverClient.js
@@ -4,17 +4,6 @@ const {
 
 const Push = new (require('pushover-notifications'))({ token: PUSHOVER_APPKEY })
 
-/**
- * Sample argument:
- * {
- *   user: 'token',
- *   message: 'omg node test',
- *   title: "Well - this is fantastic",
- *   sound: 'magic' // optional
- *   priority: 1 // optional,
- *   file: '/tmp/fancy_image.png' // optional
- * }
- */
 exports.sendNotification = async (msg) => {
   return new Promise((resolve, reject) => {
     Push.send(msg, (err, res) => {
