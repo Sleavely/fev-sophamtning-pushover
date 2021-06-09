7744a1a5ac4fe13b8248fadb44e13cc3e94bb3c8
Joakim Hedlund <contact@joakimhedlund.com>
2021-06-09 23:07:36 +0200

    Silently fail to avoid blocking the stream

--- src/utils/pushoverClient.js
+++ src/utils/pushoverClient.js
@@ -2,13 +2,22 @@ const {
   PUSHOVER_APPKEY = '',
 } = process.env
 
-const Push = new (require('pushover-notifications'))({ token: PUSHOVER_APPKEY })
+const got = require('got')
 
 exports.sendNotification = async (msg) => {
-  return new Promise((resolve, reject) => {
-    Push.send(msg, (err, res) => {
-      if (err) return reject(err)
-      resolve(res)
-    })
+  const targetUrl = 'https://api.pushover.net/1/messages.json'
+  await got.post(targetUrl, {
+    responseType: 'json',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    json: {
+      token: PUSHOVER_APPKEY,
+      ...msg,
+    },
+    // Honestly most of the time the only error we get is that the user token is wrong,
+    // and we dont want those so we'll skip looking at the response altogether.
+    throwHttpErrors: false,
   })
 }
