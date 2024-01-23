43ad380ba16699a6fb99fbb2dd781546193afe95
Joakim Hedlund <contact@joakimhedlund.com>
2024-01-23 10:42:38 +0100

    refactor(ts): convert fevClient to typescript

--- src/utils/fevClient.ts
+++ src/utils/fevClient.ts
@@ -1,17 +1,29 @@
-const got = require('got')
+import got from 'got'
 
+interface FevXhrErrorResponse {
+  error: true
+  message: string
+}
+interface FevXhrSuccessResponse<Payload = unknown> {
+  error?: false
+  payload: Payload
+}
+type FevXhrResponse<Payload = unknown> = FevXhrSuccessResponse<Payload> | FevXhrErrorResponse
+
+interface GetAddressResultOutput {
+  /**
+   * @example "Lumsheden 75"
+   */
+  strPickupAddress: string
+  /**
+  * @example "Åshammar"
+  */
+  strPickupCity: string
+}
 /**
  * Search for an address, returns the first result or undefined.
- *
- * E.g.
- * {
- *   strPickupAddress: "Lumsheden 75"
- *   strPickupCity: "Åshammar"
- * }
- *
- * @param {string} query
  */
-exports.getAddressResult = async (query) => {
+export const getAddressResult = async (query: string): Promise<GetAddressResultOutput | undefined> => {
   const queryParams = new URLSearchParams({
     // These param names seems dynamic but I'm not sure where they are derived from. fev.se deployment ID?
     'sv.12.24e83a616b997088b4cd546.route': '/',
@@ -21,7 +33,7 @@ exports.getAddressResult = async (query) => {
   const baseUrl = 'https://fev.se/atervinning/sophamtning.html'
 
   const targetUrl = `${baseUrl}?${queryParams.toString()}`
-  const { body } = await got.get(targetUrl, {
+  const { body } = await got.get<FevXhrResponse<GetAddressResultOutput[]>>(targetUrl, {
     responseType: 'json',
     headers: {
       accept: 'application/json',
@@ -32,10 +44,15 @@ exports.getAddressResult = async (query) => {
   if (body.payload.length) return body.payload[0]
 }
 
+interface GetNextPickupInput {
+  query: string
+  address: string
+  city: string
+}
 /**
  * Uses a verified address to check when the pickup will occur
  */
-exports.getNextPickup = async ({ query, address, city }) => {
+export const getNextPickup = async ({ query, address, city }: GetNextPickupInput): Promise<Date> => {
   const queryParams = new URLSearchParams({
     'sv.target': '12.24e83a616b997088b4cd546',
     'sv.12.24e83a616b997088b4cd546.route': '/',
@@ -47,7 +64,7 @@ exports.getNextPickup = async ({ query, address, city }) => {
   const baseUrl = 'https://fev.se/atervinning/sophamtning.html'
 
   const targetUrl = `${baseUrl}?${queryParams.toString()}`
-  const { body } = await got.get(targetUrl, {
+  const { body } = await got.get<FevXhrResponse<string>>(targetUrl, {
     responseType: 'json',
     headers: {
       accept: 'application/json',
@@ -57,19 +74,19 @@ exports.getNextPickup = async ({ query, address, city }) => {
 
   if (body.error) throw new Error(body.message)
 
-  return this.parseSwedishDate(body.payload)
+  return parseSwedishDate(body.payload)
 }
 
-exports.swedishMonths = ['januari', 'februari', 'mars', 'april', 'maj', 'juni', 'juli', 'augusti', 'september', 'oktober', 'november', 'december']
+export const swedishMonths = ['januari', 'februari', 'mars', 'april', 'maj', 'juni', 'juli', 'augusti', 'september', 'oktober', 'november', 'december']
 
 /**
  * @private
  * @param {string} inputStr E.g. "tisdag 15 juni"
  */
-exports.parseSwedishDate = (inputStr, currentDate = new Date()) => {
+export const parseSwedishDate = (inputStr: string, currentDate = new Date()): Date => {
   const [, inputDay, inputMonth] = inputStr.split(' ')
 
-  const targetMonth = this.swedishMonths.indexOf(inputMonth.toLowerCase())
+  const targetMonth = swedishMonths.indexOf(inputMonth.toLowerCase())
   const targetMonthPadded = `${targetMonth + 1}`.padStart(2, '0')
 
   // Cant use static because it might be next year
